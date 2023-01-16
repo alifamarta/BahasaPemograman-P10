91ff699f32931f725ccf9002aea1f50109a7257a
Alif_Amarta <alifamarta23@gmail.com>
2023-01-16 22:22:40 +0700

    Add files

---
+++ code/main.py
@@ -0,0 +1,22 @@
+txt = 'Hello World'
+
+#Hitung Jumlah Karakter
+print("Jumlah karakter txt = ",len(txt))
+
+#Ambil karakter terakhir 
+print(txt[10])
+
+#Ambil karakter index ke-2 sampai ke-4
+print(txt[2:5])
+
+#Hilangkan spasi pada text tersebut
+print(txt.replace(' ', ''))
+
+#Ubah text menjadi huruf besar
+print(txt.upper())
+
+#Ubah text menjadi huruf kecil
+print(txt.lower())
+
+#Ganti karakter H dengan karakter J
+print(txt.replace('H','J'))
